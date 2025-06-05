304863317b932a06232168c0da67cda44a84f6f2
gabriely <gabrielybraghirolli1@gmail.com>
2025-06-04 21:18:02 -0300

    Delete Menu directory

--- Menu/Estilo.java
+++
@@ -1,9 +0,0 @@
-package Menu;
-
-import java.awt.*;
-
-// Classe utilitária para estilos visuais centralizados (fonte, cor de texto etc.)
-public class Estilo {
-    public static final Font FONTE_PADRAO = new Font("Arial", Font.BOLD, 16);
-    public static final Color TEXTO_POPUP = Color.WHITE;
-}
